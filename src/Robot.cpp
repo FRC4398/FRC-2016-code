bccc0c033d6c78097ef05ce80fea448919bf761b
denchief1 <jacob@iharrand.com>
2016-03-02 15:31:53 -0500

    code update
    massive code changes

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,40 +1,79 @@
 #include "WPILib.h"
 
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
- */
-class Robot: public SampleRobot
+class Robot: public IterativeRobot
 {
+
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
+	Victor ball; // for collecting balls
+	LiveWindow *lw;
+	int autoLoopCounter;
 
 public:
 	Robot() :
-			myRobot(0, 1),	// initialize the RobotDrive to use motor controllers on ports 0 and 1
-			stick(0)
+		myRobot(0, 1),	// these must be initialized in the same order
+		stick(1),		// as they are declared above.
+		ball(2),
+		lw(LiveWindow::GetInstance()),
+		autoLoopCounter(0)
 	{
 		myRobot.SetExpiration(0.1);
 	}
+public:
+	void RobotInit() override {
+		CameraServer::GetInstance()->SetQuality(50);
+		//the camera name (ex "cam0") can be found through the roborio web interface
+		CameraServer::GetInstance()->StartAutomaticCapture("cam0");
+	}
 
-	/**
-	 * Runs the motors with arcade steering.
-	 */
-	void OperatorControl()
+
+private:
+	void AutonomousInit()
 	{
+		autoLoopCounter = 0;
+	}
+
+	void AutonomousPeriodic()
+	{
+		if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+		{
+			myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
+			autoLoopCounter++;
+			} else {
+			myRobot.Drive(0.0, 0.0); 	// stop robot
+		}
+	}
+
+	void TeleopInit()
+	{
+
+	}
+
+	void TeleopPeriodic()
+	{
+		myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+
+		if (stick.GetRawButton(3) == 1)   // for collecting and shooting
+			ball.Set(0.5);
+
+		else if (stick.GetRawButton(2) == 1)
+			ball.Set(-0.5);
+
+		else
+			ball.Set(0);
+
+
 		while (IsOperatorControl() && IsEnabled())
 		{
-			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
 			Wait(0.005);				// wait for a motor update time
 		}
 	}
 
+
+	void TestPeriodic()
+	{
+		lw->Run();
+	}
 };
 
 START_ROBOT_CLASS(Robot)
